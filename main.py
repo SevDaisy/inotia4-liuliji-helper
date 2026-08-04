018490f113d77de279931359834e265b474b5aab
sevdaisy <1052886775@qq.com>
2026-08-04 17:01:03 +0800

    perf: 整理 main.py 结果，方便不同存档分别管理

--- main.py
+++ main.py
@@ -6,31 +6,74 @@ from .pkg.SL孔位 import SL孔位
 
 print("main OK")
 
-
-# 规定到什么水平的宝石可以留.
-
 # 0621
 # 暴击 9% | 暴伤 16.5%
-# 0801
+# 0804
 # 暴击 8.6% | 暴伤 13.6% | 暴伤抵抗 25.1%
-gf = GemFilter([
-    Entry.from_str("HP吸收: 8%"),  # 混 4.3~8.6
-    Entry.from_str("暴击率: 8%"),  # 混 4.3~8.6
-    Entry.from_str("暴击伤害增加率: 15%"),  # 混 6.8~13.6
-    Entry.from_str("暴击伤害抵抗率: 23%"),  # 混 12~25.1
-    Entry.from_str("魔法抵抗率: 4%"),  # 混 4.2
-    Entry.from_str("回避率: 4%"),  # 混 4.5
-    # Entry.from_str("MP增加: 88"),  #
-])
 
-# SL宝石(gf, saveIndex=1, 背包上界=3, target=GemLevel.顶级, mode="Only Prepare")
-# SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.顶级, mode="FULL")
 
-SL宝石(gf, saveIndex=1, 背包上界=3, target=GemLevel.混沌, mode="FULL")
-# SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.混沌, mode="FULL")
+def _example():
+    gf = GemFilter([
+        Entry.from_str("暴击率: 6%"),
+        Entry.from_str("暴击伤害增加率: 10.5%"),
+        Entry.from_str("回避率: 4%"),
+        Entry.from_str("MP增加: 70"),
+        Entry.from_str("MP恢复: 4"),
+        # Entry.from_str("HP吸收: 8%"),
+        # Entry.from_str("魔法抵抗率: 4%"),
+        # Entry.from_str("暴击伤害抵抗率: 22%"),
+    ])
+    SL宝石(gf, saveIndex=1, 背包上界=3, target=GemLevel.顶级, mode="Only Prepare")
+    SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.顶级, mode="FULL")
 
-# SL强化(saveIndex=1, packIndex=1, maxSuccess=12)
-# SL强化(saveIndex=1, packIndex=1, mode="混沌普通")
-# SL强化(saveIndex=1, packIndex=1, mode="混沌关键")
+    SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.混沌, mode="FULL")
 
-# SL孔位(saveIndex=1, packIndex=1, posID="00", is可强化装备=True)
+    SL强化(saveIndex=1, packIndex=1, maxSuccess=12)
+    SL强化(saveIndex=1, packIndex=1, mode="混沌普通")
+    SL强化(saveIndex=1, packIndex=1, mode="混沌关键")
+
+    SL孔位(saveIndex=1, packIndex=1, posID="00", is可强化装备=True)
+
+
+def _0621刷暴伤():
+    gf = GemFilter([
+        # Entry.from_str("HP吸收: 8%"),  # 混 4.3~8.6
+        Entry.from_str("暴击率: 8%"),  # 混 4.3~8.6
+        Entry.from_str("暴击伤害增加率: 15%"),  # 混 6.8~13.6
+        # Entry.from_str("暴击伤害抵抗率: 22%"),  # 混 12~25.1
+        # Entry.from_str("魔法抵抗率: 4%"),  # 混 4.2
+        # Entry.from_str("回避率: 4%"),  # 混 4.5
+        # Entry.from_str("MP增加: 88"),  #
+    ])
+    SL宝石(gf, saveIndex=1, 背包上界=2, target=GemLevel.混沌, mode="FULL")
+
+
+def _0804魔导():
+    gf = GemFilter([
+        # Entry.from_str("HP吸收: 8%"),  # 混 4.3~8.6
+        Entry.from_str("暴击率: 8%"),  # 混 4.3~8.6
+        Entry.from_str("暴击伤害增加率: 15%"),  # 混 6.8~13.6
+        # Entry.from_str("暴击伤害抵抗率: 22%"),  # 混 12~25.1
+        # Entry.from_str("魔法抵抗率: 4%"),  # 混 4.2
+        # Entry.from_str("回避率: 4%"),  # 混 4.5
+        # Entry.from_str("MP增加: 88"),  #
+    ])
+    SL宝石(gf, saveIndex=1, 背包上界=2, target=GemLevel.混沌, mode="FULL")
+
+
+def _new_刺客():
+    # 新档 顶级宝石就可以留
+    gf = GemFilter([
+        Entry.from_str("暴击率: 6%"),
+        Entry.from_str("暴击伤害增加率: 10.5%"),
+        Entry.from_str("回避率: 4%"),
+        Entry.from_str("MP增加: 70"),
+        Entry.from_str("MP恢复: 4"),
+        # Entry.from_str("HP吸收: 8%"),
+        # Entry.from_str("魔法抵抗率: 4%"),
+        # Entry.from_str("暴击伤害抵抗率: 22%"),
+    ])
+    SL宝石(gf, saveIndex=2, 背包上界=2, target=GemLevel.混沌, mode="FULL")
+
+
+_new_刺客()
