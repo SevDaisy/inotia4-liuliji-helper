99d94a24e3b7431ae5209f0a5373fc68a0f2a505
sevdaisy <1052886775@qq.com>
2026-08-05 14:21:05 +0800

    SL强化: 20260804及以后版本，+30装备再次强化不会闪退了，移除跳过强30的判定

--- pkg/SL强化.py
+++ pkg/SL强化.py
@@ -105,7 +105,6 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
                     break
 
         # 一直强化到 +31
-        isKey = "关键" in mode
         oldLv = lv
         while oldLv < 31:
             打开背包()
@@ -121,9 +120,7 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
                 pClick(v["强化成功"], msg="+2 保存一下")
 
             # 有进步就保存
-            # mode == 关键, 则 +30 不保存
-            # mode != 关键, 则 +30 正常保存
-            if oldLv < lv and (not isKey or lv != 30):
+            if oldLv < lv:
                 保存()
                 oldLv = lv
                 continue
