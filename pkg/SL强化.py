d3df44710af08c851a92a7ada283e15711393721
sevdaisy <1052886775@qq.com>
2026-08-12 13:05:29 +0800

    SL强化: 支持指定强化卷轴所在位置

--- pkg/SL强化.py
+++ pkg/SL强化.py
@@ -2,10 +2,14 @@ from ..utils.auto import imgFind, ocrPaddle_V5, pClick, pSlide, toast, pin, ocrG
 from ..utils.db import loadPoint, loadRect
 
 
-# saveIndex: 第几个存档
-# packIndex: 第几个背包
-# loopTimes: <=0 无限循环
 def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal"):
+    """
+    # saveIndex: 第几个存档
+    # packIndex: 第几个背包
+    # maxSuccess: 强化成功几次
+    # mode: Normal 正常强化
+    #   else: 作为 PositionID 合法值 00、01、02、10、11、12...33, 指定混沌强化卷轴的位置
+    """
     min = 100
     short = 200
     long = 700
@@ -14,8 +18,13 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
     r["评级文本"] = loadRect("背包-装备评级文本")
     for item in [
         "主菜单页", "主菜单选项", "返回主菜单-是", "开始游戏", "跳过登录-否", f"存档{saveIndex}",
-        "菜单", "背包菜单", f"背包页{packIndex}", "背包格11", "背包格12",
-            "强化成功", "主菜单页", "保存选项", "确认保存"]:
+        "菜单", "背包菜单", f"背包页{packIndex}",
+        "背包格00", "背包格01", "背包格02", "背包格03",
+        "背包格10", "背包格11", "背包格12", "背包格13",
+        "背包格20", "背包格21", "背包格22", "背包格23",
+        "背包格30", "背包格31", "背包格32", "背包格33",
+        "强化成功", "主菜单页", "保存选项", "确认保存"
+    ]:
         v[item] = loadPoint(item)
     for k, p in v.items():
         if p is None:
@@ -32,7 +41,7 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
         pClick(v["菜单"], before=long)
 
     def 打开背包():
-        pClick(v["背包菜单"], before=short)
+        pClick(v["背包菜单"], before=short, msg="打开背包")
         # 点击 背包页{packIndex}
         if 1 < packIndex < 5:
             pClick(v[f"背包页{packIndex}"], before=short)
@@ -54,6 +63,10 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
             toast(f"异常文本：{txt[0]}")
             return -1
         return int(split[1])
+
+    # 无误触风险, 无需判断背包是否已经打开
+    pClick(v["菜单"], before=min)
+    打开背包()
 
     if mode == "Normal":
         # 普通强化
@@ -83,6 +96,16 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
         pClick(v["背包格12"], before=short)
     else:
         # 混沌卷轴强化
+        posID = 11
+        try:
+            posID = int(mode)
+        except ValueError:
+            toast("非法 mode 值, 混沌卷轴位置使用默认值 11")
+        finally:
+            if posID == 12:
+                toast("不行哦, 这个是放装备的位置")
+                return
+
         cnt = 0
         lv = 识别当前装备等级()
         if lv < 0:
@@ -92,7 +115,7 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
         if lv % 2 == 0:
             while True:
                 cnt += 1
-                pSlide(v["背包格11"], v["背包格12"], before=short)
+                pSlide(v[f"背包格{posID:02d}"], v["背包格12"], before=short)
                 lv = 识别当前装备等级()
                 if imgFind("确认", before=short):
                     pClick(v["强化成功"])
@@ -111,7 +134,7 @@ def SL强化(saveIndex=1, packIndex=1, maxRetry=0, maxSuccess=15, mode="Normal")
 
             # 强化 1 次
             cnt += 1
-            pSlide(v["背包格11"], v["背包格12"], before=short)
+            pSlide(v[f"背包格{posID:02d}"], v["背包格12"], before=short)
             lv = 识别当前装备等级()
             toast(f"B 强化 {cnt} 次, 当前等级: +{lv}")
 
