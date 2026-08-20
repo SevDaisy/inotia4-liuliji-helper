aaa23fafa35dbda134e6d6d7f4d338782f95b83c
sevdaisy <1052886775@qq.com>
2026-08-20 17:18:01 +0800

    发布 AScript 卡背包刷指定属性宝石自动化教学

--- main.py
+++ main.py
@@ -8,127 +8,14 @@ from .pkg.SL孔位 import SL孔位
 
 print("main OK")
 
-gf0621 = GemFilter([
-    Entry.from_str("暴击伤害增加率: 16.0%"),  # 最高16.5
-    # Entry.from_str("暴击率: 8.5%"), # 最高9.0
-])
-gf0804 = GemFilter([
+gf = GemFilter([
     Entry.from_str("暴击率: 8.6%"),
-    Entry.from_str("回避率: 4.5%"),
+    # 注意 一行代码的开头如果是 # 井字符, 那么这行代码就是不会有任何作用的, 是不生效的。
+    # Entry.from_str("暴击伤害增加率: 16.5%"),
+    # Entry.from_str("回避率: 4.5%"),
+    # Entry.from_str("MP恢复: 6"),
+    # Entry.from_str("魔法抵抗率: 4%"),
 ])
 
 
-def _example():
-    gf = GemFilter([
-        Entry.from_str("暴击率: 6%"),
-        Entry.from_str("暴击伤害增加率: 10.5%"),
-        Entry.from_str("回避率: 4%"),
-        Entry.from_str("MP增加: 70"),
-        Entry.from_str("MP恢复: 4"),
-        # Entry.from_str("HP吸收: 8%"),
-        # Entry.from_str("魔法抵抗率: 4%"),
-        # Entry.from_str("暴击伤害抵抗率: 22%"),
-    ])
-    SL宝石(gf, saveIndex=1, 背包上界=3, target=GemLevel.顶级, mode="Only Prepare")
-    SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.顶级, mode="FULL")
-
-    SL宝石(gf, saveIndex=1, 背包上界=1, target=GemLevel.混沌, mode="FULL")
-
-    SL强化(saveIndex=1, packIndex=1, maxSuccess=12)
-    SL强化(saveIndex=1, packIndex=1, mode="混沌普通")
-    SL强化(saveIndex=1, packIndex=1, mode="混沌关键")
-
-    SL孔位(saveIndex=1, packIndex=1, posID="00", is可强化装备=True)
-
-
-def _0621刷暴伤():
-    gf = GemFilter([
-        # Entry.from_str("HP吸收: 8%"),  # 混 4.3~8.6
-        Entry.from_str("暴击率: 8.5%"),  # 混 4.3~8.6
-        Entry.from_str("暴击伤害增加率: 16%"),  # 混 6.8~13.6
-        # Entry.from_str("暴击伤害抵抗率: 22%"),  # 混 12~25.1
-        # Entry.from_str("魔法抵抗率: 4%"),  # 混 4.2
-        # Entry.from_str("回避率: 4%"),  # 混 4.5
-        # Entry.from_str("MP增加: 88"),  #
-    ])
-    SL宝石(gf, saveIndex=1, 背包上界=4, target=GemLevel.混沌, mode="FULL")
-
-
-def _0804魔导():
-    gf = GemFilter([
-        # Entry.from_str("HP吸收: 8%"),  # 混 4.3~8.6
-        Entry.from_str("暴击率: 8%"),  # 混 4.3~8.6
-        # Entry.from_str("暴击伤害增加率: 15%"),  # 混 6.8~13.6
-        Entry.from_str("暴击伤害抵抗率: 22%"),  # 混 12~25.1
-        # Entry.from_str("魔法抵抗率: 4%"),  # 混 4.2
-        # Entry.from_str("回避率: 4%"),  # 混 4.5
-        # Entry.from_str("MP增加: 88"),  #
-    ])
-    # SL宝石(gf, saveIndex=1, 背包上界=2, target=GemLevel.混沌, mode="FULL")
-    SL孔位(saveIndex=1, packIndex=1, posID="00", is可强化装备=True)
-
-
-def _0804刺客():
-    # 新档 顶级宝石就可以留 低1 中3 高9 顶27
-    gf = GemFilter([
-        Entry.from_str("暴击率: 9%"),
-        Entry.from_str("暴击伤害增加率: 16.5%"),
-        Entry.from_str("回避率: 4.5%"),
-        # Entry.from_str("MP恢复: 5"),
-        # Entry.from_str("暴击抵抗率: 13%"),  # 后期找机会单出一个
-        # Entry.from_str("HP吸收: 8%"),
-        # Entry.from_str("魔法抵抗率: 4%"),
-    ])
-
-    SL孔位(saveIndex=2, packIndex=1, posID=f"00", is可强化装备=False)
-    # SL孔位(saveIndex=2, packIndex=1, posID=f"00", is可强化装备=True)
-    # SL孔位(saveIndex=2, packIndex=1, posID=f"01", is可强化装备=True)
-    # SL强化(saveIndex=2, packIndex=1, maxSuccess=13)
-    # SL强化(saveIndex=2, packIndex=1, mode="21")
-    # SL宝石(gf, saveIndex=2, 背包上界=1, target=GemLevel.混沌, mode="Only Prepare")
-    # SL宝石(gf, saveIndex=2, 背包上界=2, target=GemLevel.混沌, mode="FULL")
-    # SL宝石(gf, saveIndex=2, mode="bug")
-
-
-def 新档狂战():
-    # 新档 顶级宝石就可以留 低1 中3 高9 顶27
-    gf = GemFilter([
-        # Entry.from_str("暴击率: 8.6%"),
-        # Entry.from_str("暴击伤害增加率: 16.5%"),
-        # Entry.from_str("回避率: 4.5%"),
-        # Entry.from_str("MP恢复: 6"),
-        # Entry.from_str("暴击抵抗率: 13%"),  # 后期找机会单出一个
-        Entry.from_str("HP吸收: 8%"),
-        # Entry.from_str("魔法抵抗率: 4%"),
-    ])
-
-    # SL孔位(saveIndex=3, packIndex=1, posID=f"00", is可强化装备=False)
-    # SL孔位(saveIndex=3, packIndex=1, posID=f"00", is可强化装备=True)
-    # SL孔位(saveIndex=3, packIndex=1, posID=f"01", is可强化装备=True)
-    # SL强化(saveIndex=3, packIndex=1, maxSuccess=13)
-    # SL强化(saveIndex=3, packIndex=1, mode="21")
-    # SL宝石(gf, saveIndex=3, 背包上界=1, target=GemLevel.混沌, mode="Only Prepare")
-    # SL宝石(gf, saveIndex=3, 背包上界=2, target=GemLevel.混沌, mode="FULL")
-    # SL宝石(gf, saveIndex=3, mode="bug")
-
-
-def 展示参数标定():
-    for x in range(0, 4):
-        for y in range(0, 4):
-            p = v[f"背包格{x}{y}"]
-            drawCross(p, msg=f"{p.x},{p.y}", dur=60000)
-    for x in [
-        "融合器-强化孔位数量",
-        "融合器-饰品孔位数量",
-        "融合器-宝石评级文本",
-        "融合器-宝石属性文本",
-        "背包-装备评级文本",
-    ]:
-        drawRegion(r[x], msg=x, dur=60000)
-
-
-展示参数标定()
-# _0621刷暴伤()
-# _0804魔导()
-
-# 新档狂战()
+SL宝石(gf, mode="bug")
